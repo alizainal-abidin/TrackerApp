b3ea5261c13b5d97cace00ca9b6ffe1fab138da8
Ali Alhaddad <alizainal.abidin@live.com>
2020-09-17 22:09:43 +0700

    [Domain] - added core domain.

---
+++ src/Domain/Enums/IssueStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums
+{
+    public enum IssueStatus
+    {
+        Todo = 0,
+        InProgress = 1,
+        Done = 2
+    }
+}
---
+++ src/Domain/Enums/IssueType.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enums
+{
+    public enum IssueType
+    {
+        Story = 0,
+        Bug = 1,
+        Task = 2,
+
+        // SubTask = 3
+    }    
+}
