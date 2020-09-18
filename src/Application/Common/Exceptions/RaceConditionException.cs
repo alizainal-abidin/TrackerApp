145574c3f8e7fb0f5b0537c33d8ba4c6fb92d2e1
Ali Alhaddad <alizainal.abidin@live.com>
2020-09-18 09:40:16 +0700

    added custom validation exception and updated Application dependency injection config.

---
+++ src/Application/Common/Exceptions/RaceConditionException.cs
@@ -0,0 +1,26 @@
+﻿namespace Application.Common.Exceptions
+{
+    using System;
+
+    public class RaceConditionException : Exception
+    {
+        public RaceConditionException()
+        {
+        }
+
+        public RaceConditionException(string message)
+            : base(message)
+        {
+        }
+
+        public RaceConditionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public RaceConditionException(string name, object key)
+            : base($"Unable to save Entity \"{name}\" with the key: ({key}).")
+        {
+        }
+    }
+}
--- src/Application/Configuration/ApplicationConfiguration.cs
+++ src/Application/Configuration/ApplicationConfiguration.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Application.Configuration
+﻿namespace Application.Configuration
 {
-    class ApplicationConfiguration
+    using System.Reflection;
+    using Application.Common.Behaviors;
+    using AutoMapper;
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ApplicationConfiguration
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
     }
 }
