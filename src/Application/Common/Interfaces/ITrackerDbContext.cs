3f3d10bc4cc2162fddc71c7f4ec34bbe6ba65a61
Ali Alhaddad <alizainal.abidin@live.com>
2020-09-18 08:12:14 +0700

    extract ITrackerDbContext interface.

--- src/Application/Common/Interfaces/ITrackerDbContext.cs
+++ src/Application/Common/Interfaces/ITrackerDbContext.cs
@@ -1,6 +1,18 @@
 ﻿namespace Application.Common.Interfaces
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+
     public interface ITrackerDbContext
-    {        
+    {
+        DbSet<Project> Projects { get; set; }
+
+        DbSet<ProjectParticipant> ProjectParticipants { get; set; }
+
+        DbSet<Issue> Issues { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
