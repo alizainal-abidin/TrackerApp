a3b8f019ef16d3112a2337b2aaef50a7e6040e88
Ali Alhaddad <alizainal.abidin@live.com>
2020-09-19 08:21:22 +0700

    implemented Create project participant command handler.

--- src/Application/Business/Participants/Commands/CreateProjectParticipant/CreateProjectParticipantCommandHandler.cs
+++ src/Application/Business/Participants/Commands/CreateProjectParticipant/CreateProjectParticipantCommandHandler.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Application.Business.Participants.Commands.CreateProjectParticipant
+﻿namespace Application.Business.Participants.Commands.CreateProjectParticipant
 {
-    class CreateProjectParticipantCommandHandler
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Application.Common.Interfaces;
+    using Domain.Entities;
+    using MediatR;
+
+    public class CreateProjectParticipantCommandHandler : IRequestHandler<CreateProjectParticipantCommand, Guid>
     {
+        private readonly ITrackerDbContext context;
+
+        public CreateProjectParticipantCommandHandler(ITrackerDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Guid> Handle(CreateProjectParticipantCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new ProjectParticipant
+            {
+                AddedBy = request.AddedBy,
+                ProjectId = request.ProjectId,
+                UserId = request.UserId
+            };
+
+            this.context.ProjectParticipants.Add(entity);
+
+            await this.context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
     }
 }
